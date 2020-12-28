96b7eaabf36b37b2e8b01171647a3c2aa19c5093
rtorrado80ing <ricardo.torrado@gmail.com>
2020-12-28 11:29:46 -0500

    inicial

---
+++ MyGreatLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyGreatLibrary;
+
+namespace MyGreatLibrary.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 cls = new Class1();
+            Assert.AreEqual(2, cls.AddOne(1));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Class1 cls = new Class1();
+            Assert.AreEqual(0, cls.AddOne(-1));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Class1 cls = new Class1();
+            Assert.AreNotEqual(0, cls.AddOne(0));
+        }
+    }
+}
